bde6e3874c3cd5950289421934c0a49c33e03bf0
Zhanfeng <zhanfeng_xu@outlook.com>
2025-09-20 20:59:12 +0800

    Naive Bayes

--- ud120-projects/naive_bayes/nb_author_id.py
+++ ud120-projects/naive_bayes/nb_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -21,17 +21,26 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
 ##############################################################
 # Enter Your Code Here
-
-
+# predict the y, get the maxinum of P(y)/Mutiply(P(x|y)
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+t0 = time()
+pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
+accuracy = accuracy_score(pred, labels_test)
+print(accuracy)
 
 ##############################################################
 
 ##############################################################
 '''
-You Will be Required to record time for Training and Predicting 
+You Will be Required to record time for Training and Predicting
 The Code Given on Udacity Website is in Python-2
 The Following Code is Python-3 version of the same code
 '''
