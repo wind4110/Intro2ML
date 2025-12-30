f4252d882a61c683aaaed977f3f2462cf0bbd060
Zhanfeng <zhanfeng_xu@outlook.com>
2025-12-30 22:53:28 +0800

    Regression

--- ud120-projects/regression/finance_regression.py
+++ ud120-projects/regression/finance_regression.py
@@ -2,7 +2,7 @@
 
 """
     Starter code for the regression mini-project.
-    
+
     Loads up/formats a modified version of the dataset
     (why modified?  we've removed some trouble points
     that you'll find yourself in the outliers mini-project).
@@ -10,48 +10,52 @@
     Draws a little scatterplot of the training/testing data
 
     You fill in the regression code where indicated:
-"""    
+"""
 
 import os
 import sys
 import joblib
-sys.path.append(os.path.abspath("../tools/"))
+# Ensure 'tools' directory is always importable
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'tools')))
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = joblib.load( open("../final_project/final_project_dataset_modified.pkl", "rb") )
+dictionary = joblib.load( open(os.path.join(os.path.dirname(__file__), '..', 'final_project', 'final_project_dataset_modified.pkl'), "rb") )
 
 
-### list the features you want to look at--first item in the 
+### list the features you want to look at--first item in the
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys.pkl')
-target, features = targetFeatureSplit( data )
+# Use absolute path for sort_keys file
+sort_keys_path = os.path.join(os.path.dirname(__file__), '..', 'tools', 'python2_lesson06_keys.pkl')
+data = featureFormat(dictionary, features_list, remove_any_zeroes=True, sort_keys=sort_keys_path)
+target, features = targetFeatureSplit(data)
 
 ### training-testing split needed in regression, just like classification
 from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
 ### Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
+### Please name it reg, so that the plotting code below picks it up and
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
-
-
-
-
-
-
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print("Slope (coefficient):", reg.coef_)
+print("Intercept:", reg.intercept_)
+print("R^2 score on training data:", reg.score(feature_train, target_train))
+print("R^2 score on testing data:", reg.score(feature_test, target_test))
 
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
 for feature, target in zip(feature_test, target_test):
-    plt.scatter( feature, target, color=test_color ) 
+    plt.scatter( feature, target, color=test_color )
 for feature, target in zip(feature_train, target_train):
-    plt.scatter( feature, target, color=train_color ) 
+    plt.scatter( feature, target, color=train_color )
 
 ### labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
