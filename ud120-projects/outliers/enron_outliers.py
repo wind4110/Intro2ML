e80c828b2b1cb378511c38b1b21f347909718ed3
Zhanfeng <zhanfeng_xu@outlook.com>
2026-01-05 20:50:51 +0800

    Enron Outliers finished

--- ud120-projects/outliers/enron_outliers.py
+++ ud120-projects/outliers/enron_outliers.py
@@ -2,13 +2,14 @@
 import os
 import joblib
 import sys
-import matplotlib.pyplot
-sys.path.append(os.path.abspath("../tools/"))
+import matplotlib.pyplot as plt
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'tools')))
 from feature_format import featureFormat, targetFeatureSplit
 
 
 ### read in data dictionary, convert to numpy array
-data_dict = joblib.load( open("../final_project/final_project_dataset.pkl", "rb") )
+data_dict = joblib.load( open("ud120-projects/final_project/final_project_dataset.pkl", "rb") )
+data_dict.pop("TOTAL", 0) # remove outlier
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
@@ -17,3 +18,30 @@ data = featureFormat(data_dict, features)
 
 
 
+from sklearn.linear_model import LinearRegression
+import numpy as np
+reg = LinearRegression()
+salary = data[:, 0].reshape(-1, 1)
+bonus = data[:, 1].reshape(-1, 1)
+reg.fit(salary, bonus)
+print("Slope (coefficient):", reg.coef_)
+print("Intercept:", reg.intercept_)
+print("R^2 score:", reg.score(salary, bonus))
+# Identify the outlier
+max_salary = np.max(salary)
+outlier = [name for name, features in data_dict.items() if features["salary"] == max_salary]
+print("Outlier:", outlier)
+
+for name, features in data_dict.items():
+    if features["salary"] == "NaN" or features["bonus"] == "NaN":
+        continue
+    if features["salary"] > 1000000 and features["bonus"] > 5000000:
+        print("Outlier details:", name, np.abs(features['bonus']-reg.predict([[features["salary"]]])[0][0]))
+
+
+
+plt.scatter(data[:, 0], data[:, 1])
+plt.xlabel("salary")
+plt.ylabel("bonus")
+plt.plot(salary, reg.predict(salary), color="blue")
+plt.show()
